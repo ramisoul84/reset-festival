54d433ba97dfc973f286ee87c5aaacbbf6bda38a
Rami <rami.suliman@mail.ru>
2025-03-26 09:13:05 +0300

    slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import { Swiper } from 'swiper/types';
 
 @Component({
   selector: 'app-slider',
@@ -10,4 +12,17 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
 })
 export class SliderComponent {
   @Input() images: string[] = [];
+
+
+  ngAfterViewInit() {
+    const swiper:any = document.querySelector('.swiper-container');
+
+    swiper.on('slideChangeTransitionEnd', () => {
+      if (swiper.isEnd) {
+        // Force update pagination when reaching the end
+        swiper.pagination.update();
+        swiper.pagination.render();
+      }
+    });
+  }
 }
